c9789e98dc4da9b946a754f4910b724e3e9488b3
Gustavo Hortega <gustavohortega80@gmail.com>
2025-03-18 10:15:30 -0400

    Adicione rota de depósitos ao index com middleware de autenticação jwt.

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,11 +3,14 @@ const express = require('express');
 const statusRouter = require('./status');
 const usuariosRouter = require('./usuarios');
 const authRouter = require('./auth');
+const depositosRouter = require('./depositos');
+const passport = require('passport');
 
 const router = express.Router();
 
 require('./auth/jwt');
 
+router.use('/depositos', passport.authenticate('jwt', { session: false }), depositosRouter);
 router.use('/status', statusRouter);
 router.use('/usuarios', usuariosRouter);
 router.use('/auth', authRouter);
