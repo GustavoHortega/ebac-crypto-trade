e8d9490eecb9f6d98679de76b093c0bf2fc4c78b
Gustavo Hortega <gustavohortega80@gmail.com>
2025-04-08 14:30:13 -0400

    Importe rota de saques.

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const passport = require('passport');
 
 const statusRouter = require('./status');
 const usuariosRouter = require('./usuarios');
 const authRouter = require('./auth');
 const depositosRouter = require('./depositos');
-const passport = require('passport');
+const saquesRouter = require('./saques');
 
 const router = express.Router();
 
 require('./auth/jwt');
 
 router.use('/depositos', passport.authenticate('jwt', { session: false }), depositosRouter);
+router.use('/saques', passport.authenticate('jwt', { session: false }), saquesRouter);
 router.use('/status', statusRouter);
 router.use('/usuarios', usuariosRouter);
 router.use('/auth', authRouter);
