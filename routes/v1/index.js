21b416ac671257ded0727f92ecbff9df3708a6bc
Gustavo Hortega <hortegadev.tech@gmail.com>
2026-07-03 12:22:51 -0400

    feat: Adiciona rota para buscar cotações de criptomoedas

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -8,12 +8,14 @@ const usuariosRouter = require('./usuarios');
 const authRouter = require('./auth');
 const depositosRouter = require('./depositos');
 const saquesRouter = require('./saques');
+const cotacoesRouter = require('./cotacoes');
 
 const router = express.Router();
 
 router.use('/status', statusRouter); // Rota de status do sistema
 router.use('/usuarios', usuariosRouter); // Rota de usuários
 router.use('/auth', authRouter); // Rota de autenticação
+router.use('/cotacoes', cotacoesRouter); // Rota de cotações
 router.use('/depositos', passport.authenticate('jwt', { session: false }), depositosRouter); // Rota de depósitos
 router.use('/saques', passport.authenticate('jwt', { session: false }), saquesRouter); // Rota de saques
 
