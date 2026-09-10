eb034e2b0582b8b5bb54e540fe52c67038d99d96
Gustavo Hortega <gustavohortega80@gmail.com>
2026-09-10 12:53:29 -0400

    feat: Adiciona constantes CNPJ e RESERVA_MINIMA e implementa script de seed para a base de dados

---
+++ constants.js
@@ -0,0 +1,7 @@
+const CNPJ = '14.722.388/0001-49';
+const RESERVA_MINIMA = 150000;
+
+module.exports = {
+  CNPJ,
+  RESERVA_MINIMA,
+};
--- seed.js
+++ seed.js
@@ -1,1 +1,20 @@
-// popula base de dados
+require('dotenv').config();
+
+const { mongoose } = require('mongoose');
+
+const { CNPJ, RESERVA_MINIMA } = require('./constants');
+const { Corretora, connect } = require('./models');
+
+(async () => {
+    await connect();
+
+    await Corretora.findOneAndUpdate({
+        cnpj: CNPJ
+    }, {
+        caixa: RESERVA_MINIMA
+    }, {
+        upsert: true // Insere o documento se não existir
+    });
+
+    await mongoose.disconnect();
+})();
