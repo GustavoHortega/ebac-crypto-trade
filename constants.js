1c6bfc41be52471f94987d10bad08394a352c5ec
Gustavo Hortega <gustavohortega80@gmail.com>
2026-09-11 12:47:01 -0400

    feat: Adiciona constante TAXA_DE_TROCA ao módulo de constantes

--- constants.js
+++ constants.js
@@ -1,7 +1,9 @@
 const CNPJ = '14.722.388/0001-49';
 const RESERVA_MINIMA = 150000;
+const TAXA_DE_TROCA = 0.005; // 0.5% de taxa de corretagem
 
 module.exports = {
   CNPJ,
   RESERVA_MINIMA,
+  TAXA_DE_TROCA
 };
